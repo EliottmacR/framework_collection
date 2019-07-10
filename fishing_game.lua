29962737c2e43b0b64712dbd9b1a03c781054d68
Eliott <eliotts@protonmail.com>
2019-07-10 12:22:11 +0200

    a

--- fishing_game.lua
+++ fishing_game.lua
@@ -43,7 +43,7 @@ function _update()
 end
 
 function _draw()
-  -- cls(1)
+  cls(1)
   
   -- glyph(0x03, 32, 32, 16, 16, 2*t(), 2, 3)
   
@@ -60,7 +60,7 @@ function _draw()
   -- end
   
   -- name of this game
-    -- print("Fishing Game", GW / 2 - sugar.gfx.str_px_width("Fishing Game")/2, 2, flr(t()* 3))  
+    print("Fishing Game", GW / 2 - sugar.gfx.str_px_width("Fishing Game")/2, 2, flr(t()* 3))  
     
   -- objects
   -- for _, obj in pairs(_objects) do
@@ -68,11 +68,11 @@ function _draw()
   -- end
   
  -- circfill(x, y, 7, 2)
-  -- local a = atan2(btnv"cur_x" - x, btnv"cur_y" - y)
-  -- outlined_glyph(0x20, x, y, 16, sgn(cos(a)) * (16 + 2*sin(t())), a, 2, 3, 0)
+  local a = atan2(btnv"cur_x" - x, btnv"cur_y" - y)
+  outlined_glyph(0x20, x, y, 16, sgn(cos(a)) * (16 + 2*sin(t())), a, 2, 3, 0)
   
  -- circ(btnv("cur_x"), btnv("cur_y"), btn("cur_rb") and 6 or btn("cur_lb") and 12 or 3, 4)
-  -- outlined_glyph(0x00, btnv("cur_x"), btnv("cur_y"), 8 + 8 * btnv("cur_lb"), 8 + 8 * btnv("cur_rb"), 0, 4, 5, 0)
+  outlined_glyph(0x00, btnv("cur_x"), btnv("cur_y"), 8 + 8 * btnv("cur_lb"), 8 + 8 * btnv("cur_rb"), 0, 4, 5, 0)
   
   
   
