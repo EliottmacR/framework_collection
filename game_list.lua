b5a12362dfcc98618949d80ef1a51d149e4c6b83
Eliott <eliotts@protonmail.com>
2019-07-10 09:14:06 +0200

    a

--- game_list.lua
+++ game_list.lua
@@ -12,10 +12,10 @@ games = {
 
 
 
-function get_path_from_id(game_id)
-  if not game_id then return end
-  return games[game_id].path  
-end
+-- function get_path_from_id(game_id)
+  -- if not game_id then return end
+  -- return games[game_id].path  
+-- end
   
 function get_id_from_name(game_name)
   if not game_name then return end
