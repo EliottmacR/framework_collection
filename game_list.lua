10b3ffd7c6facb2dbfc5714f9393343562184636
Eliott <eliotts@protonmail.com>
2019-07-10 09:17:40 +0200

    a

--- game_list.lua
+++ game_list.lua
@@ -6,7 +6,7 @@
   -- id : game
 -- }
 
-games = {
+_games = {
   {name = "fishing_game", path = "https://raw.githubusercontent.com/EliottmacR/FishingGame/master/game_template.lua"}
 }
 
@@ -14,12 +14,12 @@ games = {
 
 -- function get_path_from_id(game_id)
   -- if not game_id then return end
-  -- return games[game_id].path  
+  -- return _games[game_id].path  
 -- end
   
 -- function get_id_from_name(game_name)
   -- if not game_name then return end
-  -- for ind, game in pairs(games) do
+  -- for ind, game in pairs(_games) do
     -- if game.name == game_name then return ind end  
   -- end  
 -- end
